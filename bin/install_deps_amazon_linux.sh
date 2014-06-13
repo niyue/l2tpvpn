ce6d8b8e43f340907ccaf31fe3b121a46817b099
Ni Yue <niyue.com@gmail.com>
2014-06-13 21:49:15 +0800

    Add support for Amazon Linux.

---
+++ bin/install_deps_amazon_linux.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+echo "The following script will install puppet, git via yum, and clone l2tpvpn module from Github, and you have to enter an admin user's password to do this"
+sudo yum-config-manager --enable epel
+sudo yum install puppet
+sudo yum install git
+git clone https://github.com/niyue/l2tpvpn.git
+echo "***************************************"
+if [ -d "./l2tpvpn" ]; then
+	echo "Puppet and git are successfully installed, and l2tpvpn puppet module is successfully cloned from Github. Please modify 'shared_key', 'credentials' in l2tpvpn/tests/init.pp. You can set 'shared_key' to any value you like and VPN username/password in 'credentials' table"
+else
+	echo "Something went wrong. It is possible puppet/git is not successuflly installed due to network problem. Please check the installation log above for details."
+fi
--- bin/install_module.sh
+++ bin/install_module.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-puppet module build
+puppet module build .
 sudo puppet module install ./pkg/niyue-l2tpvpn-0.1.0.tar.gz --force --target-dir /usr/share/puppet/modules
